10bbc6747a1bd85781a45b66d0077288cda27605
BonacePatrick <patrickruboneza@gmail.com>
2024-10-07 22:09:45 +0200

    Increase Responsiveness & add better UX

--- components/products/CartModel.tsx
+++ components/products/CartModel.tsx
@@ -151,7 +151,7 @@ const CartModal = () => {
                       />
                     </div>
                     <div>
-                      <h3 className="font-medium whitespace-nowrap">
+                      <h3 className="font-medium">
                         {item.name}
                       </h3>
                       <p className="text-sm text-gray-500">{item.price} Rwf</p>
